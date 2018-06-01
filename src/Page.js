00dd1b453a104964f1d6a62088047207eb2f7187
Dino Trojak <dinotrojak@gmail.com>
2018-06-01 12:57:54 +0200

    moved description to input elements

--- src/Page.js
+++ src/Page.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import * as React from 'react';
 import { Typography, Button, Icons } from '@kiwicom/orbit-components';
 
 import App from './App';
@@ -19,7 +19,7 @@ type State = {
     calendarVisible: bool
 }
 
-class Page extends Component<null, State> {
+class Page extends React.Component<null, State> {
     state={
       calendarVisible: false,
     }
@@ -34,23 +34,25 @@ class Page extends Component<null, State> {
         <Container>
           {!calendarVisible ? <Title>Travelanywhere</Title> : <Title>Set a departure date</Title>}
           {!calendarVisible ?
-            <Inputs onClick={this.showCalendar}>
-              <Typography size="large" type="secondary">Try Sightseeing Europe</Typography>
-              <RightContent>
-                <Typography>Round trip</Typography>
-                <Spacer />
-                <span style={{ marginRight: '10px' }}>
-                  <Typography>Departure - Return</Typography>
-                </span>
-                <Button Icon={Search} onlyIcon onClick={() => {}} />
-              </RightContent>
-            </Inputs>
+            <React.Fragment>
+              <Inputs onClick={this.showCalendar}>
+                <Typography size="large" type="secondary">Try Sightseeing Europe</Typography>
+                <RightContent>
+                  <Typography>Round trip</Typography>
+                  <Spacer />
+                  <span style={{ marginRight: '10px' }}>
+                    <Typography>Departure - Return</Typography>
+                  </span>
+                  <Button Icon={Search} onlyIcon onClick={() => {}} />
+                </RightContent>
+              </Inputs>
+              <p style={{ marginTop: '15px' }}>
+                <Typography size="large">Search Flights, trains & buses from Brno, Prague and Vienna</Typography>
+              </p>
+            </React.Fragment>
           :
             <App />
           }
-          <p style={{ marginTop: '15px' }}>
-            <Typography size="large">Search Flights, trains & buses from Brno, Prague and Vienna</Typography>
-          </p>
         </Container>
       );
     }
